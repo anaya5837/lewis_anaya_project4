7c7f3235a577d0bbf15d32330a71a0b8fb4db97b
anaya5837 <72808504+anaya5837@users.noreply.github.com>
2021-02-03 20:55:48 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ while True:
     print(input.rotation(Rotation.PITCH))
     if input.rotation(Rotation.PITCH) < 0:
         light.set_all(light.rgb(100, 0, 100))
-    elif input.rotation(Rotation.PITCH) > 100:
+    elif input.rotation(Rotation.PITCH) > 0:
         light.set_all(light.rgb(0, 100, 100))
     else:
         light.set_all(light.rgb(0, 0, 0))
--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ while (true) {
     console.log(input.rotation(Rotation.Pitch))
     if (input.rotation(Rotation.Pitch) < 0) {
         light.setAll(light.rgb(100, 0, 100))
-    } else if (input.rotation(Rotation.Pitch) > 100) {
+    } else if (input.rotation(Rotation.Pitch) > 0) {
         light.setAll(light.rgb(0, 100, 100))
     } else {
         light.setAll(light.rgb(0, 0, 0))
