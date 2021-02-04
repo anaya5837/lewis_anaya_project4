3d4388b5a1a232bb779923c56419e1ad66832f0a
anaya5837 <72808504+anaya5837@users.noreply.github.com>
2021-02-03 20:46:18 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+while (true) {
+    console.log(input.rotation(Rotation.Pitch))
+    if (input.rotation(Rotation.Pitch) < 0) {
+        light.setAll(light.rgb(100, 0, 100))
+    } else if (input.rotation(Rotation.Pitch) > 100) {
+        light.setAll(light.rgb(0, 100, 100))
+    } else {
+        light.setAll(light.rgb(0, 0, 0))
+    }
+    
+}
