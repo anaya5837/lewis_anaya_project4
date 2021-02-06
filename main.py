5d081c814254a3a29663ecd5a2da1f03fcfa32c4
anaya5837 <72808504+anaya5837@users.noreply.github.com>
2021-02-06 14:58:17 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-
+light.set_brightness(50)
 while True:
     print(input.rotation(Rotation.PITCH))
     if input.rotation(Rotation.PITCH) <= -2:
         light.set_all(light.rgb(100, 0, 100))
-        light.clear
         pause(300)
-        light.clear
+        light.clear()
+        pause(300)
     elif input.rotation(Rotation.PITCH) >= 3:
-        light.set_all(light.rgb(0, 100, 100))
-        light.clear
+        light.set_all(light.rgb(255, 200, 0))
         pause(300)
-        light.clear
+        light.clear()
+        pause(300)
     else:
         light.set_all(light.rgb(0, 0, 0))
--- main.ts
+++ main.ts
@@ -1,15 +1,16 @@
+light.setBrightness(50)
 while (true) {
     console.log(input.rotation(Rotation.Pitch))
     if (input.rotation(Rotation.Pitch) <= -2) {
         light.setAll(light.rgb(100, 0, 100))
-        light.clear
         pause(300)
-        light.clear
+        light.clear()
+        pause(300)
     } else if (input.rotation(Rotation.Pitch) >= 3) {
-        light.setAll(light.rgb(0, 100, 100))
-        light.clear
+        light.setAll(light.rgb(255, 200, 0))
         pause(300)
-        light.clear
+        light.clear()
+        pause(300)
     } else {
         light.setAll(light.rgb(0, 0, 0))
     }
